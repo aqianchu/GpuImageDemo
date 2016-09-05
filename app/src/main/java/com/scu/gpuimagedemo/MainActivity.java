440b9d4bab54a764894ea60886ae4a3e82247152
zhangqianchu <zhangqianchu@360.cn>
2016-09-05 12:28:02 +0800

    可以使用滤镜了

--- app/src/main/java/com/scu/gpuimagedemo/MainActivity.java
+++ app/src/main/java/com/scu/gpuimagedemo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.scu.gpuimagedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+import jp.co.cyberagent.android.gpuimage.GPUImageGrayscaleFilter;
 
-    @Override
+public class MainActivity extends Activity {
+    private GPUImage gpuImage;
+    //显示处理结果
+    private ImageView resultIv;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultIv = (ImageView) findViewById(R.id.resultIv);
+        Bitmap bitmap = null;
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.test);
+        // 使用GPUImage处理图像
+        gpuImage = new GPUImage(this);
+        gpuImage.setImage(bitmap);
+        gpuImage.setFilter(new GPUImageGrayscaleFilter());
+        bitmap = gpuImage.getBitmapWithFilterApplied();
+        //显示处理后的图片
+        resultIv.setImageBitmap(bitmap);
     }
 }
